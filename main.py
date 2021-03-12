ef8323e79ba5659b91deab8f93f986ac7c6b8048
lakiert <aleksandra.lakiert@gmail.com>
2021-03-12 13:06:29 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+# zad1
+# print('\n zad 1')
+# lista = ['pilka nozna', 'siatkowka', 'tenis stolowy']
+# print(lista)
+# lista.reverse()
+# print(lista)
+# lista.append('koszykowka')
+# lista.append('pilka reczna')
+# print(lista)
+
+
+# zad2
+# print('\n zad 2')
+# slownik={'br':'bieżącego roku', 'tzw':'tak zwany', 'itp':'i tym podobne'}
+# print(slownik)
+
+
+# zad3
+# print('\n zad 3')
+# slownik={'m':'mario', 'nfs':'need for speed', 'mc':'minecraft'}
+# print(slownik)
+# print(len(slownik))
+
+# zad4
+# print('\n zad 4')
+# zdanie=input('wprowadz zdanie: ')
+# print(zdanie)
+# print(zdanie.count('a'))
+
+
+# zad5
+# print('\n zad 5')
+# import sys as system
+# system.stdout.write("podaj liczbe a: ")
+# a = system.stdin.readline()
+# system.stdout.write("podaj liczbe b: ")
+# b = system.stdin.readline()
+# system.stdout.write("podaj liczbe c: ")
+# c = system.stdin.readline()
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# suma=pow(a,b)+c
+# system.stdout.write(str(suma))
+
+# zad6
+# print('\n zad 6')
+# a = input('liczba a: ')
+# b = input('liczba b: ')
+# c = input('liczba c: ')
+#
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# if a > b:
+#     if a > c:
+#         print('liczba', str(a), 'jest najwieksza')
+# elif b > a:
+#     if b > c:
+#         print('liczba', str(b), 'jest najwieksza')
+# elif c > a:
+#     if c > b:
+#         print('liczba', str(c), 'jest najwieksza')
+# else:
+#     print('inna sytuacja')
+
+# ??????????????????????????????????????
+
+# zad7
+# print('\n zad 7')
+# lista=[5, 3.2, 12, 5.5]
+#
+# for x in lista:
+#     print (x*x)
+
+# zad8
+# print('\n zad 8')
+# a=0
+# lista = []
+# while a!=10:
+#     liczba = input('wprowac ' + str(a+1) + ' liczbe:')
+#     liczba = int(liczba)
+#     if liczba % 2 == 0:
+#         lista.append(liczba)
+#     a += 1
+# print(lista)
+
+# zad9
+# print('\n zad 9')
+# lista = [1, 2, 3, 4, 5]
+# for x in lista:
+#     if x % 2 != 0:
+#         print('EEEEEE')
+#     else:
+#         print('E')
+
+# zad10
+# print('\n zad 10')
+
+# ??????????????
+
+
+
+
